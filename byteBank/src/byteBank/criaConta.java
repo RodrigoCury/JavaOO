ace3b4bee33f43b980d52999eb62ea1531867f9a
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-08 15:27:46 -0300

    Learned TryCatch

--- byteBank/src/byteBank/criaConta.java
+++ byteBank/src/byteBank/criaConta.java
@@ -3,16 +3,20 @@ package byteBank;
 public class criaConta {
 
 	public static void main(String[] args) throws Exception {
-				
-		Conta contaRodrigo = new Conta(6, 1, new Cliente("Rodrigo", "111.222.333-44", "Programador"));
-		contaRodrigo.depositar(200);
 		
-		Conta contaLuigi = new Conta(96, 2, new Cliente("Luigi", "111.222.333-44", "Programador"));		
-		
-		contaRodrigo.transfere(200, contaLuigi);
-		
-		contaRodrigo.meMostra();
-		contaLuigi.meMostra();
+		try {
+			Conta contaRodrigo = new Conta(61, 1, new Cliente("Rodrigo", "111.222.333-44", "Programador"));
+			contaRodrigo.depositar(200);
+			
+			Conta contaLuigi = new Conta(96, 2, new Cliente("Luigi", "111.222.333-44", "Programador"));		
+			
+			contaRodrigo.transfere(200, contaLuigi);
+			
+			contaRodrigo.meMostra();
+			contaLuigi.meMostra();			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
