9545ee4eecf75ac74b55eee210539b467b072921
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-08 14:26:37 -0300

    Conta Class

---
+++ byteBank/src/byteBank/Conta.java
@@ -0,0 +1,45 @@
+package byteBank;
+
+public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+	
+	public Conta(int agencia, int numero, String titular) {
+		this.agencia = agencia;
+		this.numero = numero;
+		this.titular = titular;
+		this.saldo = 0;
+	}
+
+	public boolean sacar(double valorASerSacado) {
+		if (valorASerSacado <= this.saldo && valorASerSacado > 0.0) {
+			this.saldo -= valorASerSacado;
+			return true;
+		}
+		return false;
+	}
+
+	public void depositar(double valorADepositar) {
+		if (valorADepositar > 0) {
+			this.saldo += valorADepositar;
+		}
+	}
+
+	public void meMostra() {
+		System.out.println("Agência: " + this.agencia);
+		System.out.println("Titular: " + this.titular);
+		System.out.println("Numero: " + this.numero);
+		System.out.println("Saldo: " + this.saldo);
+	}
+
+	public boolean transfere(double valorATransferir, Conta contaRecebedora) {
+		if (valorATransferir <= this.saldo && valorATransferir > 0.0) {
+			this.saldo -= valorATransferir;
+			contaRecebedora.saldo += valorATransferir;
+			return true;
+		}
+		return false;
+	}
+}
