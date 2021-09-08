39e01dddbf5e7363f2d4494dcb430666ed2cfc97
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-08 14:40:49 -0300

    Adicionado Classe Cliente

--- byteBank/src/byteBank/criaConta.java
+++ byteBank/src/byteBank/criaConta.java
@@ -3,10 +3,14 @@ package byteBank;
 public class criaConta {
 
 	public static void main(String[] args) {
-		Conta contaRodrigo = new Conta(146, 1, "Rodrigo");
+		
+		Cliente rodrigo = new Cliente("Rodrigo", "111.222.333-44", "Programador");
+		Cliente luigi = new Cliente("Luigi", "111.222.333-44", "Programador");
+		
+		Conta contaRodrigo = new Conta(146, 1, rodrigo);
 		contaRodrigo.depositar(200);
 		
-		Conta contaLuigi = new Conta(146, 2, "Luigi");		
+		Conta contaLuigi = new Conta(146, 2, luigi);		
 		
 		contaRodrigo.transfere(200, contaLuigi);
 		
